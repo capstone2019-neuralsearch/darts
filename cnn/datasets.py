88ceca80e1535068168e7d7531136184290f3a68
Dylan Randle <dylanrandle@rogers.com>
2019-10-04 11:04:25 -0400

    add train/test flag, and placeholder for GrapheneKirigami

--- cnn/datasets.py
+++ cnn/datasets.py
@@ -6,28 +6,33 @@ import torchvision.datasets as dset
 VALID_DSET_NAMES = {
     'CIFAR': ['cifar', 'cifar10', 'cifar-10'],
     'MNIST': ['mnist'],
-    'FashionMNIST': ['fashionmnist', 'fashion-mnist', 'mnistfashion']
+    'FashionMNIST': ['fashionmnist', 'fashion-mnist', 'mnistfashion'],
+    'GrapheneKirigami': ['graphene', 'graphenekirigami', 'graphene-kirigami', 'kirigami']
 }
 
-def load_dataset(args):
+def load_dataset(args, train=True):
     """ function to load datasets (e.g. CIFAR10, MNIST, FashionMNIST, Graphene) """
     dset_name = args.dataset.lower().strip()
 
     if dset_name in VALID_DSET_NAMES['CIFAR']:
         train_transform, valid_transform = utils._data_transforms_cifar10(args)
-        train_data = dset.CIFAR10(root=args.data, train=True, download=True, transform=train_transform)
+        data = dset.CIFAR10(root=args.data, train=train, download=True, transform=train_transform)
 
     elif dset_name in VALID_DSET_NAMES['MNIST']:
         # TODO: add transforms
-        train_data = dset.MNIST(root=args.data, train=True, download=True, transform=None)
+        data = dset.MNIST(root=args.data, train=train, download=True, transform=None)
 
     elif dset_name in VALID_DSET_NAMES['FashionMNIST']:
         # TODO: add transforms
-        train_data = dset.FashionMNIST(root=args.data, train=True, download=True, transform=None)
+        data = dset.FashionMNIST(root=args.data, train=train, download=True, transform=None)
+
+    elif dset_name in VALID_DSET_NAMES['GrapheneKirigami']:
+        # TODO: implement loading graphene
+        raise NotImplementedError()
 
     else:
         exc_str = 'Unable to match provided dataset name: {}'.format(dset_name)
         exc_str += '\nValid names are case-insensitive elements of: {}'.format(VALID_DSET_NAMES)
         raise RuntimeError(exc_str)
 
-    return train_data
+    return data
