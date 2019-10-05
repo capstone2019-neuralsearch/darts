c61fa888c2e461785b007a41748e8593643cbbbe
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2019-10-05 15:10:01 -0400

    added best cell for mnist and fashion-mnist to genotypes.py (output of NAS in log file)

--- cnn/genotypes.py
+++ cnn/genotypes.py
@@ -76,3 +76,52 @@ DARTS_V2 = Genotype(normal=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv
 
 DARTS = DARTS_V2
 
+# MSE 2019-10-05: copy the best discovered architecture from NAS for mnist and fashion-mnist
+# The way to get these is to look at the log.txt in the directory, e.g. search-EXP-20191004-160833
+# train_search.py periodically logs out the best genotype discovered.  copy / paste the last one here.
+
+# Best architecture for mnist
+MNIST = Genotype(
+	normal=[
+	('max_pool_3x3', 0), 
+	('skip_connect', 1), 
+	('max_pool_3x3', 0), 
+	('sep_conv_3x3', 2), 
+	('sep_conv_3x3', 3), 
+	('sep_conv_3x3', 2), 
+	('sep_conv_5x5', 4), 
+	('sep_conv_3x3', 3)], 
+	normal_concat=range(2, 6), 
+	reduce=[
+	('avg_pool_3x3', 0), 
+	('avg_pool_3x3', 1), 
+	('avg_pool_3x3', 0), 
+	('avg_pool_3x3', 1), 
+	('avg_pool_3x3', 0), 
+	('dil_conv_5x5', 3), 
+	('avg_pool_3x3', 0), 
+	('dil_conv_5x5', 4)], 
+	reduce_concat=range(2, 6))
+	
+# Best architecture for fashion-mnist
+FASHION_MNIST = Genotype(
+	normal=[
+	('skip_connect', 0), 
+	('sep_conv_5x5', 1), 
+	('dil_conv_5x5', 1), 
+	('skip_connect', 0), 
+	('sep_conv_5x5', 3), 
+	('sep_conv_3x3', 0), 
+	('sep_conv_3x3', 4), 
+	('sep_conv_5x5', 1)], 
+	normal_concat=range(2, 6), 
+	reduce=[
+	('avg_pool_3x3', 0), 
+	('dil_conv_3x3', 1), 
+	('avg_pool_3x3', 0), 
+	('skip_connect', 2), 
+	('avg_pool_3x3', 0), 
+	('dil_conv_5x5', 2), 
+	('avg_pool_3x3', 0), 
+	('skip_connect', 2)], 
+	reduce_concat=range(2, 6))
