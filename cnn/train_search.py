a4fbd89e27428a13b8b9b343db8cd3c3e7dea21b
Dylan Randle <dylanrandle@rogers.com>
2019-10-14 12:59:26 -0400

    save the arch weights, once per epoch

--- cnn/train_search.py
+++ cnn/train_search.py
@@ -113,9 +113,13 @@ def main():
     genotype = model.genotype()
     logging.info('genotype = %s', genotype)
 
-    # change from print statement to logging (saves to file)
-    logging.info('\nalphas_normal = {}'.format(F.softmax(model.alphas_normal, dim=-1)))
-    logging.info('\nalphas_reduce = {}'.format(F.softmax(model.alphas_reduce, dim=-1)))
+    # saving & logging weights for architecture
+    normal_weights = F.softmax(model.alphas_normal, dim=-1)
+    reduce_weights = F.softmax(model.alphas_reduce, dim=-1)
+    logging.info('\nalphas_normal = {}'.format(normal_weights))
+    logging.info('\nalphas_reduce = {}'.format(reduce_weights))
+    np.save(os.path.join(args.save, '%d_normal.npz' % epochs), normal_weights.data.cpu().numpy())
+    np.save(os.path.join(args.save, '%d_reduce.npz' % epochs), reduce_weights.data.cpu().numpy())
 
     # training
     train_acc, train_obj = train(train_queue, valid_queue, model, architect, criterion, optimizer, lr, is_regression=is_regression)
