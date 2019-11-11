ca95d16117e247a61f181ee92a9bf6c40f402e40
Dylan Randle <dylanrandle@rogers.com>
2019-11-11 14:21:39 -0500

    clean up args and give some comments to help understand whats important

--- cnn/train.py
+++ cnn/train.py
@@ -21,33 +21,44 @@ from genotypes import GENOTYPE_TBL
 from sklearn.metrics import r2_score
 
 parser = argparse.ArgumentParser("darts")
+
+# NON-PERFORMANCE RELATED
 parser.add_argument('--dataset', type=str, default='cifar', help='name of the dataset to use (e.g. cifar, mnist, graphene)')
 parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
-parser.add_argument('--val_portion', type=float, default=0.1, help='portion of validation data')
-parser.add_argument('--batch_size', type=int, default=64, help='batch size')
-parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer; one of SGD or Adam')
-parser.add_argument('--learning_rate', type=float, default=0.001, help='init learning rate')
-parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
-parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
 parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
 parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
-parser.add_argument('--epochs', type=int, default=100, help='num of training epochs')
+parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
+parser.add_argument('--save', type=str, default='EXP', help='experiment name')
+parser.add_argument('--seed', type=int, default=0, help='random seed')
+parser.add_argument('--random', action="store_true", default=False, help='train a random cell')
+
+# PERFORMANCE RELATED
+
+## HIGHLY IMPORTANT (TUNE THESE)
+parser.add_argument('--learning_rate', type=float, default=1e-3, help='init learning rate')
+parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
 parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
 parser.add_argument('--layers', type=int, default=20, help='total number of layers')
-parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
+parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path probability') # this is dropout
+parser.add_argument('--arch', type=str, default='DATASET',
+                    help='which architecture to use; default is lookup by dataset name')
+
+## MEDIUM IMPORTANT
+parser.add_argument('--epochs', type=int, default=100, help='num of training epochs')
+parser.add_argument('--batch_size', type=int, default=64, help='batch size')
+parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer; one of SGD or Adam')
+parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
+parser.add_argument('--fc1_size', type=int, default=1024, help='number of units in fully connected layer 1')
+parser.add_argument('--fc2_size', type=int, default=1024, help='number of units in fully connected layer 2')
+
+## LESS IMPORTANT
+parser.add_argument('--val_portion', type=float, default=0.1, help='portion of validation data')
+parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
 parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
 parser.add_argument('--auxiliary_weight', type=float, default=0.4, help='weight for auxiliary loss')
 parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
 parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
-parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path probability')
-parser.add_argument('--save', type=str, default='EXP', help='experiment name')
-parser.add_argument('--seed', type=int, default=0, help='random seed')
-parser.add_argument('--arch', type=str, default='DATASET',
-                    help='which architecture to use; default is lookup by dataset name')
-parser.add_argument('--fc1_size', type=int, default=1024, help='number of units in fully connected layer 1')
-parser.add_argument('--fc2_size', type=int, default=1024, help='number of units in fully connected layer 2')
-parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
-parser.add_argument('--random', action="store_true", default=False, help='train a random cell')
+
 args = parser.parse_args()
 
 args.save = 'eval-{}-{}-{}'.format(args.dataset, args.save, time.strftime("%Y%m%d-%H%M%S"))
--- cnn/train_search.py
+++ cnn/train_search.py
@@ -21,39 +21,49 @@ from sklearn.metrics import r2_score
 
 
 parser = argparse.ArgumentParser("darts")
+
+# NON-PERFORMANCE RELATED
 parser.add_argument('--dataset', type=str, default='cifar', help='name of the dataset to use (e.g. cifar, mnist, graphene)')
 parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
-parser.add_argument('--batch_size', type=int, default=64, help='batch size')
-parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer; one of SGD or Adam')
-parser.add_argument('--primitives', type=str, default='Default',
-                    help='set of primitive operations for arch search; defined in genotypes.py')
-parser.add_argument('--learning_rate', type=float, default=1e-3, help='init learning rate')
-parser.add_argument('--learning_rate_min', type=float, default=1e-4, help='min learning rate')
-parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
-parser.add_argument('--weight_decay', type=float, default=1e-6, help='weight decay')
 parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
 parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
-parser.add_argument('--epochs', type=int, default=50, help='num of training epochs')
-parser.add_argument('--init_channels', type=int, default=16, help='num of init channels')
-parser.add_argument('--layers', type=int, default=8, help='total number of layers')
 parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
-parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
-parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
-# the below arg is not used anywhere in this script, nor is it used in any of the loaded modules
-# parser.add_argument('--drop_path_prob', type=float, default=0.3, help='drop path probability')
 parser.add_argument('--save', type=str, default='EXP', help='experiment name')
 parser.add_argument('--seed', type=int, default=2, help='random seed')
-parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
-parser.add_argument('--train_portion', type=float, default=0.5, help='portion of training data')
-parser.add_argument('--unrolled', action='store_true', default=True, help='use one-step unrolled validation loss')
+
+# PERFORMANCE-RELATED
+
+## HIGHLY IMPORTANT (TUNE THESE)
+parser.add_argument('--init_channels', type=int, default=16, help='num of init channels')
+parser.add_argument('--layers', type=int, default=8, help='total number of layers')
+parser.add_argument('--learning_rate', type=float, default=1e-3, help='init learning rate')
+parser.add_argument('--learning_rate_min', type=float, default=1e-4, help='min learning rate')
+parser.add_argument('--weight_decay', type=float, default=1e-6, help='weight decay')
 parser.add_argument('--arch_learning_rate', type=float, default=1e-3, help='learning rate for arch encoding')
 parser.add_argument('--arch_weight_decay', type=float, default=1e-6, help='weight decay for arch encoding')
+parser.add_argument('--cell_steps', type=int, default=4, help='number of steps in a cell (see model_search.Network)')
+parser.add_argument('--cell_multiplier', type=int, default=4, help='multiplier for a cell (see model_search.Network)')
+parser.add_argument('--primitives', type=str, default='Default',
+                    help='set of primitive operations for arch search; defined in genotypes.py')
+
+## MEDIUM IMPORTANT
+parser.add_argument('--epochs', type=int, default=50, help='num of training epochs')
+parser.add_argument('--batch_size', type=int, default=64, help='batch size')
+parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer; one of SGD or Adam')
+parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--fc1_size', type=int, default=1024, help='number of units in fully connected layer 1')
 parser.add_argument('--fc2_size', type=int, default=1024, help='number of units in fully connected layer 2')
 parser.add_argument('--gz_regression', action='store_true', default=False,
                     help='run GalaxyZoo as a standard regression (default False: use custom GZ decision tree)')
-parser.add_argument('--cell_steps', type=int, default=4, help='number of steps in a cell (see model_search.Network)')
-parser.add_argument('--cell_multiplier', type=int, default=4, help='multiplier for a cell (see model_search.Network)')
+
+## LESS IMPORTANT
+parser.add_argument('--train_portion', type=float, default=0.5, help='portion of training data')
+parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
+parser.add_argument('--unrolled', action='store_true', default=True, help='use one-step unrolled validation loss')
+parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
+parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
+# the below arg is not used anywhere in this script, nor is it used in any of the loaded modules
+# parser.add_argument('--drop_path_prob', type=float, default=0.3, help='drop path probability')
 args = parser.parse_args()
 
 args.save = 'search-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
@@ -68,7 +78,6 @@ logging.getLogger().addHandler(fh)
 
 # Get normalized dataset name
 dataset = DSET_NAME_TBL[args.dataset.lower().strip()]
-
 
 # If the default set of primitives is requested, use the normalized name of the dataset
 primitives_name = dataset if args.primitives == 'Default' else args.primitives
